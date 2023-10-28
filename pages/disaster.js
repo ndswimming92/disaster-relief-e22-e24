3e7a3d174a4dba82e9ac71b7695e29933cef4027
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-10-28 12:57:03 -0500

    still updating disaster and disastercards

--- pages/disaster.js
+++ pages/disaster.js
@@ -1,5 +1,28 @@
-const disaster = () => (
-  <div>disaster</div>
-);
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { getDisasters } from '../api/disasterData';
+import DisasterCard from '../components/DisasterCard';
 
-export default disaster;
+export default function ShowDisasters() {
+  const [disasters, setDisasters] = useState([]);
+
+  const getAllTheDisasters = () => {
+    getDisasters().then(setDisasters);
+  };
+
+  useEffect(() => {
+    getAllTheDisasters();
+  }, []);
+
+  return (
+    <>
+      <div className="text-center my-4">
+        <Link href="/disaster/about" passHref>
+          <Button>Details</Button>
+        </Link>
+        {disasters.map((disaster) => <DisasterCard key={disaster.id} disasterObj={disaster} onUpdate={getAllTheDisasters} />)}
+      </div>
+    </>
+  );
+}
