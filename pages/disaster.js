d3fea39b40a63581cc9fda9721591a3c5708e442
Steve Butler <butlersteve.c@gmail.com>
2023-10-31 20:18:10 -0500

    style the view edit delete buttons on card

--- pages/disaster.js
+++ pages/disaster.js
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { useAuth } from '../utils/context/authContext';
 import { getAllDisasters } from '../api/disasterData';
 import DisasterCard from '../components/DisasterCard';
-import { checkUser } from '../utils/auth';
 
 export default function ShowDisasters() {
   const [disasters, setDisasters] = useState([]);
-  const { user } = useAuth();
-  const [authUser, setAuthUser] = useState({});
 
   const getAllTheDisasters = () => {
     getAllDisasters().then(setDisasters);
@@ -16,7 +12,6 @@ export default function ShowDisasters() {
 
   useEffect(() => {
     getAllTheDisasters();
-    checkUser(user.uid).then((data) => setAuthUser(data));
   }, []);
 
   return (
@@ -25,7 +20,7 @@ export default function ShowDisasters() {
         <title>Disasters</title>
       </Head>
       <div className="text-center my-4 d-flex gap-3 flex-column">
-        {disasters.map((disaster) => <DisasterCard key={disaster.id} disasterObj={disaster} onUpdate={getAllTheDisasters} adminUser={authUser} />)}
+        {disasters.map((disaster) => <DisasterCard key={disaster.id} disasterObj={disaster} onUpdate={getAllTheDisasters} />)}
       </div>
     </>
   );
