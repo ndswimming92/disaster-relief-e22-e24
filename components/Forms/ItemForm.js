05e7c2173e3adae2c7837896f274e495afd9e8d3
Demansell <demansell2016@gmail.com>
2023-11-02 19:20:52 -0500

    fixed the item form

--- components/Forms/ItemForm.js
+++ components/Forms/ItemForm.js
@@ -9,15 +9,16 @@ import { Button } from 'react-bootstrap';
 import { useAuth } from '../../utils/context/authContext';
 import { getAllDisasters } from '../../api/disasterData';
 import { createItem } from '../../api/itemData';
+import { getAllCategories } from '../../api/categoryData';
 
 const initialState = {
   itemName: '',
   count: '',
-  id: null,
 };
 // this is a comment
 function ItemForm({ itemObj }) {
   const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [formInput, setFormInput] = useState(initialState);
   const { user } = useAuth();
 
@@ -26,6 +27,10 @@ function ItemForm({ itemObj }) {
 
     if (itemObj.id) setFormInput(itemObj);
   }, [itemObj, user]);
+
+  useEffect(() => {
+    getAllCategories().then((data) => setCategories(data));
+  }, []);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -40,60 +45,55 @@ function ItemForm({ itemObj }) {
     e.preventDefault();
     const payload = { ...formInput };
     console.warn('Payload: ', payload);
-    createItem(payload);
+    createItem(payload)
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FloatingLabel controlId="floatingInput1" label="Enter the donation Name" className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="Enter the donation Name"
-          name="itemName"
-          value={formInput.itemName}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
 
-      <FloatingLabel controlId="floatingInput1" label="Enter the amount in which you would be willing to donate" className="mb-3">
-        <Form.Control
-          type="text"
-          placeholder="If giving currency please use USD"
-          name="count"
-          value={formInput.count}
-          onChange={handleChange}
-          required
-        />
-      </FloatingLabel>
+    <><FloatingLabel controlId="floatingSelect" label="Categories">
+      <Form.Select
+        aria-label="Categories"
+        name="categoryId"
+        onChange={handleChange}
+        className="mb-3"
+        value={formInput.categoryId} // FIXME: modify code to remove error
+        required
+      >
+        <option value="">Select a category to put this Item on</option>
+        {categories.map((category) => (
+          <option
+            key={category.id}
+            value={category.id}
+          >
+            {category.categoryName}
+          </option>
+        ))}
+      </Form.Select>
+    </FloatingLabel><Form onSubmit={handleSubmit}>
+        <FloatingLabel controlId="floatingInput1" label="Enter the donation Name" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Enter the donation Name"
+            name="itemName"
+            value={formInput.itemName}
+            onChange={handleChange}
+            required />
+        </FloatingLabel>
 
-      <FloatingLabel controlId="floatingSelect" label="Categories">
-        <Form.Select
-          aria-label="Categories"
-          name="categoryId"
-          onChange={handleChange}
-          className="mb-3"
-          value={formInput.categoryId} // FIXME: modify code to remove error
-          required
-        >
-          <option value="">Select a category to put this Item on</option>
-          {
-            items.map((category) => (
-              <option
-                key={category.id}
-                value={category.id}
-              >
-                {category.categoryName}
-              </option>
-            ))
-          }
-        </Form.Select>
-      </FloatingLabel>
+        <FloatingLabel controlId="floatingInput1" label="Enter the amount in which you would be willing to donate" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="If giving currency please use USD"
+            name="count"
+            value={formInput.count}
+            onChange={handleChange}
+            required />
+        </FloatingLabel>
 
-      <div>
-        <Button type="submit" variant="outline-warning" style={{ marginBottom: '30px' }}>{itemObj.id ? 'Update' : 'Post'} Your Donation Item</Button>
-      </div>
-    </Form>
+        <div>
+          <Button type="submit" variant="outline-warning" style={{ marginBottom: '30px' }}>{itemObj.id ? 'Update' : 'Post'} Your Donation Item</Button>
+        </div>
+      </Form></>
   );
 }
 
